245bdd327537c40adc6a610bbe2afa7210ae4b28
moedeera <82620264+moedeera@users.noreply.github.com>
2023-05-31 23:02:34 -0600

    committ

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,30 +1,160 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Test.css";
 import axios from "axios";
 
 export const Test = () => {
-  useEffect(() => {
-    const sendData = async () => {
-      try {
-        const url = "https://librum-dev.azurewebsites.net/api/login";
-        const data = {
-          Email: "js1@protonmail.com",
-          Password: "Abc123",
-        };
-        const headers = {
-          "X-Version": "1.0",
-        };
+  const [loading, setLoading] = useState(true);
 
-        const response = await axios.post(url, data, { headers });
-        console.log("POST request sent successfully");
-        console.log("Response:", response.data);
+  const [logged, setLogged] = useState(false);
+  const [userInfo, setUserinfo] = useState(null);
+  const [userData, showUserData] = useState(false);
+  const [bookCollections, showBooks] = useState(false);
+  //   useEffect(() => {
+  //     const sendData = async () => {
+  //       try {
+  //         const url = "https://librum-dev.azurewebsites.net/api/login";
+  //         const data = {
+  //           Email: "js1@protonmail.com",
+  //           Password: "Abc123",
+  //         };
+  //         const headers = {
+  //           "X-Version": "1.0",
+  //         };
+
+  //         const response = await axios.post(url, data, { headers });
+  //         console.log("POST request sent successfully");
+  //         console.log("Response:", response.data);
+  //       } catch (error) {
+  //         console.error("Error sending POST request:", error);
+  //       }
+  //     };
+
+  //     sendData();
+  //   }, []);
+
+  const login = () => {
+    const token = localStorage.getItem("token");
+
+    (async () => {
+      setLoading(true);
+      const data = {
+        Email: "js1@protonmail.com",
+        Password: "Abc123",
+      };
+
+      const headers = {
+        "X-Version": "1.0",
+        Authorization: `Bearer ${token}`,
+      };
+
+      try {
+        const response = await axios.post(
+          "https://librum-dev.azurewebsites.net/api/login",
+          data,
+          { headers }
+        );
+        console.log(response.data);
+        localStorage.setItem("token", response.data);
+        setLogged(true);
+        setLoading(false);
       } catch (error) {
-        console.error("Error sending POST request:", error);
+        console.error(error);
+      }
+    })();
+  };
+
+  useEffect(() => {
+    const checkLocalStorage = () => {
+      setLoading(true);
+      const token = localStorage.getItem("token");
+      if (token) {
+        console.log("token is present ");
+        setLoading(false);
+        setLogged(true);
+      }
+      if (!token) {
+        console.log("no token");
+        setLoading(false);
       }
     };
 
-    sendData();
+    checkLocalStorage();
   }, []);
 
-  return <div className=" test-page container">Test</div>;
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.log("Error: No token");
+      return;
+    }
+
+    const headers = {
+      "X-Version": "1.0",
+      Authorization: `Bearer ${token}`,
+    };
+
+    console.log(headers);
+
+    try {
+      // axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      const response = await axios.get(
+        "https://librum-dev.azurewebsites.net/api/user"
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className=" test-page container">
+      {loading && (
+        <div className="standard-loading">
+          <div className="lds-default">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>{" "}
+        </div>
+      )}
+
+      {logged ? (
+        <>
+          <div className="profile-page">
+            You are logged in
+            <button
+              className="profile-btn"
+              onClick={() => {
+                showUserData(!userData);
+                getUserData();
+              }}
+            >
+              {" "}
+              Fetch User Data
+            </button>
+            {showUserData && <div>Books</div>}
+          </div>
+        </>
+      ) : (
+        <div>
+          <button
+            onClick={() => {
+              login();
+            }}
+          >
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
---
+++ src/Components/Test/log.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const log = () => {
+  //   useEffect(() => {
+  //     const sendData = async () => {
+  //       try {
+  //         const url = "https://librum-dev.azurewebsites.net/api/login";
+  //         const data = {
+  //           Email: "js1@protonmail.com",
+  //           Password: "Abc123",
+  //         };
+  //         const headers = {
+  //           "X-Version": "1.0",
+  //         };
+
+  //         const response = await axios.post(url, data, { headers });
+  //         console.log("POST request sent successfully");
+  //         console.log("Response:", response.data);
+  //       } catch (error) {
+  //         console.error("Error sending POST request:", error);
+  //       }
+  //     };
+
+  //     sendData();
+  //   }, []);
+  return <div>log</div>;
+};

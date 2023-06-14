1c0e26b97a6b221249ca8a07de41d459225e64df
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-06-14 16:29:29 +0200

    Added privacy policy

--- src/Pages/PrivacyPolicy/PrivacyPolicy.js
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.js
@@ -4,8 +4,8 @@ import Template from "./Template";
 const PrivacyPolicy = (props) => (
   <div className="main-container">
     <h1 className="title">Privacy Policy</h1>
-    <h3 className="sub-title">Last Updated Jun 1st, 2023 </h3>
-    <p className="effective-date">Effective Date: [Date]</p>
+    <h3 className="sub-title">Last Updated Jul 14th, 2023 </h3>
+    <p className="effective-date">Effective Date: On official release (Not Yet)</p>
     <Template meta={props} />
   </div>
 );

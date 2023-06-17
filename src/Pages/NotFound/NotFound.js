cc70b39c1f3807cea7d1ca69789f72b1cc57fd4c
jr-cast <castanos@protonmail.com>
2023-06-17 12:22:22 -0600

    Update 404 pagee according to commnents

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,13 +1,40 @@
 import "./NotFound.css";
-import notFoundImage from "./404.png"
+import { useContext } from "react";
+import { SiteContext } from "../../Context/Context";
+import notFoundImage from "./pngegg.png"
 
-const NotFound = () => (
-  <div className="not-found-container">
-    <img
-      src={notFoundImage}
-      className="not-found-image"
-      alt="not-found" />
-  </div>
-);
+const NotFound = () => {
+  const { bg } = useContext(SiteContext);
+
+  return (
+    <div
+      className="not-found-container"
+      style={
+        bg === "light" ?
+          { backgroundColor: "white" } : { backgroundColor: "#282C34" }
+      }
+    >
+      <h1>404</h1>
+      <h3
+        className="title"
+        style={
+          bg === "light" ?
+            { color: "#282C34" } : { color: "white" }
+        }
+      >
+        Whoops!
+        <br />
+        Page not found
+      </h3>
+      <div className="not-found-image-container">
+        <img
+          src={notFoundImage}
+          className="not-found-image"
+          alt="not-found"
+        />
+      </div>
+    </div >
+  );
+};
 
 export { NotFound };

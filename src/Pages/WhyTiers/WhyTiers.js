4b3b0ae7907232b738b3428ce4db63021c8fac04
jr-cast <castanos@protonmail.com>
2023-06-20 11:22:38 -0600

    Added whyTiers placelholder route and empty view

---
+++ src/Pages/WhyTiers/WhyTiers.js
@@ -0,0 +1,11 @@
+import "./WhyTiers.css"
+
+const WhyTiers = () => (
+  <div className="main-container">
+    <h1 className="title">
+      WHY TIERS VIEW HERE
+    </h1>
+  </div>
+);
+
+export { WhyTiers };

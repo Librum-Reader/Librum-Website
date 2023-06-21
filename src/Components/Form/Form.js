6d213c0ea73ce8270e78235434d2dfc5b8e28d2d
David Francisco <davidfco.pozo@gmail.com>
2023-06-21 18:43:03 +0200

    Recaptcha hook added to get token
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,14 @@
 import React, { useContext, useRef } from "react";
 import { SiteContext } from "../../Context/Context";
 import useFormSubmit from "../../hooks/useFormSubmit";
-import ReCAPTCHA from "react-google-recaptcha";
-import Axios from "axios";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 
 import pic4 from "../../Pages/Support/pic4.svg";
 import pic from "./message.svg";
 import "./Form.css";
 import { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
 
 export const Form = () => {
   const { bg } = useContext(SiteContext);
@@ -15,23 +16,39 @@ export const Form = () => {
   const name = useRef();
   const email = useRef();
   const message = useRef();
-  const captchaRef = useRef();
+  //const captchaRef = useRef();
 
   const [validToken, setValidToken] = useState({});
+  const [recaptcha, setRecaptcha] = useState("");
 
   const verifyToken = async (token) => {
     let APIResponse = [];
 
     try {
-      let response = await Axios.post(
+      /*  let response = await Axios.post(
         `https://www.google.com/recaptcha/api/siteverify`,
         {
           reCAPTCHA_TOKEN: token,
           Secret_Key: process.env.REACT_APP_reCAPTCHA_SITE_KEY,
         }
+      ); */
+      let response = await fetch(
+        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.REACT_APP_reCAPTCHA_SECRET_KEY}&response=${token}`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "*",
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            /*
+            "Access-Control-Request-Headers": "x-custom-header",
+            */
+          },
+        }
       );
-
-      APIResponse.push(response["data"]);
+      console.log(response);
+      //APIResponse.push(response);
       return APIResponse;
     } catch (error) {
       console.log(error);
@@ -52,23 +69,23 @@ export const Form = () => {
     email.current.value = "";
     message.current.value = ""; */
 
-    let token = captchaRef.current.getValue();
-    captchaRef.current.reset();
-    console.log(token);
+    /* let token = captchaRef.current.getValue();
+    captchaRef.current.reset(); */
+    //console.log(token);
 
-    if (token) {
-      let tokenValid = await verifyToken(token);
-      console.log(tokenValid);
+    if (recaptcha) {
+      let tokenValid = await verifyToken(recaptcha);
+      console.log("Valid token=>", tokenValid);
       setValidToken(tokenValid);
 
       if (tokenValid === true) {
-        console.log("verified");
+        /* console.log("verified");
         const values = {
           name: name.current.value,
           email: email.current.value,
           details: message.current.value,
         };
-        await submitForm(values);
+        await submitForm(values); */
 
         name.current.value = "";
         email.current.value = "";
@@ -79,6 +96,30 @@ export const Form = () => {
     }
   };
 
+  const { executeRecaptcha } = useGoogleReCaptcha();
+
+  // Create an event handler so you can call the verification on button click event or form submit
+  const handleReCaptchaVerify = useCallback(async () => {
+    if (!executeRecaptcha) {
+      console.log("Execute recaptcha not yet available");
+      return;
+    }
+
+    const token = await executeRecaptcha("SupportPage");
+    console.log("Token", token);
+    // Do whatever you want with the token
+  }, [executeRecaptcha]);
+
+  // You can use useEffect to trigger the verification as soon as the component being loaded
+  useEffect(() => {
+    handleReCaptchaVerify();
+  }, [handleReCaptchaVerify]);
+
+  const onChange = (value) => {
+    console.log("Captcha value:", value);
+    setRecaptcha(value);
+  };
+
   return (
     <div
       style={{ margin: "100px auto 0", paddingBottom: "100px" }}
@@ -86,7 +127,8 @@ export const Form = () => {
     >
       <div className="support-page-contact-form">
         <h2>Contact Us</h2>
-        <form onSubmit={handleSubmit}>
+
+        <form onSubmit={handleReCaptchaVerify}>
           <p
             style={
               bg === "light"
@@ -169,10 +211,6 @@ export const Form = () => {
               Submit{" "}
             </button>
           </div>
-          <ReCAPTCHA
-            ref={captchaRef}
-            sitekey="6LfgqrUmAAAAALgjv0VBnNKNbCQ5H64gMYKtGRwz"
-          />
         </form>
       </div>
       <div className="support-page-contact-image">

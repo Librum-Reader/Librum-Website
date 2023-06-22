9a8d1c5772f3a45590d59f75fbba304313297702
David Lazarescu <69865187+DavidLazarescu@users.noreply.github.com>
2023-06-22 12:19:03 +0200

    Added public site key

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <SiteContextProvider>
       <GoogleReCaptchaProvider
-        reCaptchaKey={process.env.REACT_APP_reCAPTCHA_SITE_KEY}
+        reCaptchaKey={6LcCs7UmAAAAAMyOwTpzSJp-Xc65HtMumO8pHYUg}
       >
         <div className="App">
           <Router>

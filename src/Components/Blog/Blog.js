0637d1d829627fbc306feb82bc8d998d3de36f57
jr-cast <castanos@protonmail.com>
2023-06-30 11:34:24 -0600

    feat: Add create blog route and component

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,17 +1,47 @@
 import React, { useState } from 'react';
 import template from './template';
+import axios from 'axios';
 import './Blog.css';
 
 const Renderer = () => {
   const [htmlCode, setHtmlCode] = useState(template);
-  // const [postTemplate, setPostTemplate] = useState(template);
-
   const handleHtmlChange = (event) => {
     setHtmlCode(event.target.value);
   }
 
+  const createPost = (e) => {
+    e.preventDefault();
+    console.log("create");
+    const blogUrl = "https://librum-dev.azurewebsites.net/api/blog";
+    const contentUrl = "https://librum-dev.azurewebsites.net/api/blog/content/eb02d931-df3d-4e86-9dcb-08db7983f63b";
+    const blogData = {
+      Title: "Some Title Here",
+      Introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    }
+    const contentData = new FormData();
+    contentData.append("content", htmlCode);
+
+    const jsonConfig = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const multidataConfig = {
+      headers: {
+        'Content-Disposition': 'form-data; name="file"; filename="file.txt"',
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+    axios.post(blogUrl, blogData, jsonConfig)
+      .then(() => {
+        axios.post(contentUrl, contentData, multidataConfig)
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className='outer'>
+    <form className='outer'>
       <div className='blog'>
         <div className='html'>
           <h2 className='heading'>Write post</h2>
@@ -31,9 +61,14 @@ const Renderer = () => {
         </div>
       </div>
       <div className='button-container'>
-        <button className='btn'>Create Post</button>
+        <button
+          className='btn'
+          onClick={(e) => createPost(e)}
+        >
+          Create Post
+        </button>
       </div>
-    </div>
+    </form>
   );
 }
 
--- src/Components/Blog/template.js
+++ src/Components/Blog/template.js
@@ -1,26 +1,34 @@
 const template = `
-<h1>Title Here</h2>
-<h2>Subitle Here</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
-<br>
+  <h1>Title Here</h2>
+  <h2>Subitle Here</h2>
 
-<h4>1.1 Section Title<h4>
+  <br>
 
-<p>
-Aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
-</p>
+  <h4>1.1 Section Title<h4>
 
-<br>
+  <p>
+  Aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+  </p>
 
-<h4>1.1 Section Title<h4>
+  <br>
 
-<p>
-Aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
-</p>
+  <h4>1.1 Section Title<h4>
 
-<br>
-
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYcnYIswRRn1KHXP58oRRG1VVWFnnvFfJPpNMPMRA&s" alt="test" />
+  <p>
+  Aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur
+  </p>
+  
+</body>
+</html>
 `;
 
 export default template;

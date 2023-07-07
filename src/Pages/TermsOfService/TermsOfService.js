4f10ee8bb4acedf46a10bcf39bec790ed7513fb5
Koncz Róbert <115461826+Kaer88@users.noreply.github.com>
2023-07-07 11:41:15 +0200

    Fix tos site (#16)
    * set container minheight
    * footer behaviour fixed
    * move padding to child element
    * fix font colors
    * cleanup code

--- src/Pages/TermsOfService/TermsOfService.js
+++ src/Pages/TermsOfService/TermsOfService.js
@@ -1,10 +1,30 @@
 import "./TermsOfService.css";
 import { Template } from "./Template";
+import { useContext } from "react";
+import { SiteContext } from "../../Context/Context";
 
-const TermsOfService = (props) => (
-  <div className="main-container">
-    <Template meta={props} />
-  </div>
-);
+const TermsOfService = (props) => {
+  const { bg } = useContext(SiteContext)
+  
+  return (
+    <div className="container" style={
+      bg === "light"
+        ? {
+          backgroundColor: "white",
+          color: "var(--color-primary0)",
+          paddingBottom: "100px;",
+        }
+        : {
+          backgroundColor: "#282c34",
+          color: "white",
+          paddingBottom: "100px",
+        }
+    }
+       >
+      <Template meta={props} />
+    </div >
+  );
+
+}
 
 export { TermsOfService };

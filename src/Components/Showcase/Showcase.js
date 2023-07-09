2dbe340a96a641baae597cbcb8fc5ad977c62dd3
odics <odics@vcu.edu>
2023-07-09 22:32:13 +0100

    Added wave under showcase

--- src/Components/Showcase/Showcase.js
+++ src/Components/Showcase/Showcase.js
@@ -24,45 +24,50 @@ export const Showcase = ({ image1 }) => {
   }, []);
 
   return (
-    <div className="showcase-container">
-      <div className="image sm">
-        <img src={image1} alt="" />
-      </div>
-      <div className="info-section">
-        <div className="info-section-text">
-          <h1>A Simple and Free E-Book Reader</h1>
-          <p
-            style={
-              bg === "light"
-                ? { color: "var(--color-primary0)" }
-                : {
-                    color: "white",
-                  }
-            }
-          >
-            A clean and simple way to read your books on any device and build
-            your own online library
-          </p>
+    <div className="showcase-wrapper">
+      <div className="showcase-container">
+        <div className="image sm">
+          <img src={image1} alt="" />
         </div>
-        <div className="info-section-button">
-          <button className="btn btn-secondary btn-sc">
-            <FaLinux size="1.5rem" style={{ color: "var(--color-primary0)" }} />
-            <p>Download</p>
-          </button>
-          <a
-            href={"https://github.com/Librum-Reader/Librum"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="btn btn-primary btn-sc">
-              <AiFillGithub size="1.5rem" />
-              GitHub
+        <div className="info-section">
+          <div className="info-section-text">
+            <h1>A Simple and Free E-Book Reader</h1>
+            <p
+              style={
+                bg === "light"
+                  ? { color: "var(--color-primary0)" }
+                  : {
+                      color: "white",
+                    }
+              }
+            >
+              A clean and simple way to read your books on any device and build
+              your own online library
+            </p>
+          </div>
+          <div className="info-section-button">
+            <button className="btn btn-secondary btn-sc">
+              <FaLinux
+                size="1.5rem"
+                style={{ color: "var(--color-primary0)" }}
+              />
+              <p>Download</p>
             </button>
-          </a>
+            <a
+              href={"https://github.com/Librum-Reader/Librum"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn btn-primary btn-sc">
+                <AiFillGithub size="1.5rem" />
+                GitHub
+              </button>
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="image lg" ref={container}>
-        {/* <img src={image1} alt="" /> */}
+        <div className="image lg" ref={container}>
+          {/* <img src={image1} alt="" /> */}
+        </div>
       </div>
     </div>
   );

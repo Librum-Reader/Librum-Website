6a761a928e57245024d9a5828aabdf3e0d24a61a
Kaer88 <robert.koncz88@gmail.com>
2023-07-10 21:50:11 +0200

    align last updated

--- src/Pages/PrivacyPolicy/Template.js
+++ src/Pages/PrivacyPolicy/Template.js
@@ -2,11 +2,8 @@ const Template = () => (
   <div className="main-template">
     <h1 className="title legal-text-title">PRIVACY POLICY</h1>
 
-    <p>Last updated: June 10, 2023</p>
-    <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information
-      when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
-    <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and
-      use of information in accordance with this Privacy Policy.</p>
+    <p  className="last-updated">Last updated: June 10, 2023</p>
+    
     <h2 className="legal-text-subtitle">Interpretation and Definitions</h2>
 
     <h3 className="legal-text-sec-subtitle indent-on-subtitle">Interpretation</h3>

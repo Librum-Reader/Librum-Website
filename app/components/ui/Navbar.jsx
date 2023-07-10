447bbdb0b3f5b78af4ca93ec229f06d0b14a5ec7
Christian Moronta <cmoronta26@gmail.com>
2023-07-10 19:42:24 -0400

    Fix gradient not showing up on navbar

--- app/components/ui/Navbar.jsx
+++ app/components/ui/Navbar.jsx
@@ -4,39 +4,32 @@ import { Box, Flex, Spacer, Button } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Navbar = () => {
-  return (
-    <Flex
-      mt="2rem"
-      maxW="1300px"
-      w="100%"
-      justify="center"
-      mx="auto"
-      className="navbar"
-    >
-      <Box>Librum</Box>
-      <Spacer />
-      <Flex gap="2rem">
-        <Link href="/">
-          <Button colorScheme="teal" variant="ghost">
-            HOME
-          </Button>
-        </Link>
-        <Link href="/contact">
-          <Button colorScheme="teal" variant="ghost">
-            CONTACT
-          </Button>
-        </Link>
-        <Link href="/news">
-          <Button colorScheme="teal" variant="ghost">
-            NEWS
-          </Button>
-        </Link>
-        <Button colorScheme="teal" variant="solid">
-          LOGIN
-        </Button>
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex pt="2rem" w="100%" pl={4} pr={4} justify="center" className="navbar">
+			<Box>Librum</Box>
+			<Spacer />
+			<Flex gap="2rem">
+				<Link href="/">
+					<Button colorScheme="teal" variant="ghost">
+						HOME
+					</Button>
+				</Link>
+				<Link href="/contact">
+					<Button colorScheme="teal" variant="ghost">
+						CONTACT
+					</Button>
+				</Link>
+				<Link href="/news">
+					<Button colorScheme="teal" variant="ghost">
+						NEWS
+					</Button>
+				</Link>
+				<Button colorScheme="teal" variant="solid">
+					LOGIN
+				</Button>
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Navbar;
--- app/layout.jsx
+++ app/layout.jsx
@@ -2,33 +2,32 @@
 import { Inter } from "next/font/google";
 import { Lato } from "next/font/google";
 import Navbar from "./components/ui/Navbar";
-import { Flex } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
-import Features from "./components/sections/Features";
+import Container from "./components/ui/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 const lato = Lato({
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
+	subsets: ["latin"],
+	weight: ["100", "300", "400", "700", "900"],
 });
 const theme = extendTheme({
-  fonts: {
-    body: lato,
-  },
+	fonts: {
+		body: lato,
+	},
 });
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <ChakraProvider>
-          <Flex direction="column" justifyContent="center" className="layout">
-            <Navbar />
-            {children}
-          </Flex>
-        </ChakraProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<ChakraProvider>
+					<Container>
+						<Navbar />
+						{children}
+					</Container>
+				</ChakraProvider>
+			</body>
+		</html>
+	);
 }

e823da1dd59ee109bb77c0eacff7e29aa345ec92
Kaer88 <robert.koncz88@gmail.com>
2023-07-10 20:24:14 +0200

    implement context, default class, include Template

--- src/Components/Cookies/Cookies.js
+++ src/Components/Cookies/Cookies.js
@@ -1,32 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cookies.css";
+import { SiteContext } from "../../Context/Context";
+import { Template } from "./Template";
 
-export function Cookies() {
+export function Cookies(props) {
+  const { bg } = useContext(SiteContext);
+
+
   return (
-    <main className="cookies-container">
-      <header>
-        <h1 className="cookies-title">Cookies</h1>
-        <article>
-          <section className="cookies-text">
-            <p>
-              We use cookies on our website to provide you with the most relevant experience by remembering your
-              preferences and repeat visits.
-            </p>
-          </section>
-          <section className="cookies-text">
-            <p>
-              By clicking "Accept", you consent to the use of ALL the cookies. However, you may visit "Cookie Settings"
-              to provide a controlled consent.
-            </p>
-          </section>
-          <section className="cookies-text">
-            <p>
-              For more information about how we use cookies, please refer to our{" "}
-              <a href="/privacypolicy">Privacy Policy</a>.
-            </p>
-          </section>
-        </article>
-      </header>
-    </main>
+    <div className="container" style={
+      bg === "light"
+        ? {
+          backgroundColor: "white",
+          color: "var(--color-primary0)",
+          paddingBottom: "100px;",
+        }
+        : {
+          backgroundColor: "#282c34",
+          color: "white",
+          paddingBottom: "100px",
+        }
+    }>
+      <Template meta={props} />
+
+    </div>
   );
 }

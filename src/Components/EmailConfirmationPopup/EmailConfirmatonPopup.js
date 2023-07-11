ee81c495b99bd9f8720ed3a67259f0baf80cc9ec
Alex <alextrieu52@gmail.com>
2023-07-11 07:10:47 -0700

    added css for mobile responsiveness

--- src/Components/EmailConfirmationPopup/EmailConfirmatonPopup.js
+++ src/Components/EmailConfirmationPopup/EmailConfirmatonPopup.js
@@ -8,18 +8,17 @@ export function EmailConfirmatonPopup() {
       <div className={styles.modalContent}>
         <header className={styles.modalHeader}>
           <h2 className={styles.modalTitle}>Librum</h2>
-          <button className={styles.exitButton}>X</button>
         </header>
         <main className={styles.modalBody}>
           <img src={icon} className={styles.popupIcon} alt="icon" />
           <h3>Verify your email</h3>
           <p className={styles.modalDescription}>
-            Verifying your email gives you access to more features on Librum. Click the button below to join our
-            worldwide community of readers
+            <span className={styles.modelText}>Verifying your email gives you access to more features on Librum.</span>{" "}
+            <span className={styles.modelText}>Click the button below to join our worldwide community of readers</span>
           </p>
         </main>
         <footer className={styles.modalFooter}>
-          <button>CONFIRM EMAIL</button>
+          <button className={styles.confirmationBtn}>CONFIRM EMAIL</button>
         </footer>
       </div>
     </section>

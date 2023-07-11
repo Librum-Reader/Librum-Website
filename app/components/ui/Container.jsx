741441b31e3b14ed1cb2725c67c1af55b7742e31
odics <odics@vcu.edu>
2023-07-11 23:12:36 +0100

    Changed nav to only show gradient on home pg

--- app/components/ui/Container.jsx
+++ app/components/ui/Container.jsx
@@ -1,15 +1,24 @@
+"use client";
+
 const { Flex } = require("@chakra-ui/react");
+import { usePathname } from "next/navigation";
 
 const Container = (props) => {
-	return (
-		<Flex
-			direction="column"
-			bgGradient="linear(to-br, #9494ee, #8f98ee, #8c9ded,#89a0ec,#87a4ea,#7f9add,#7691d0,#6e87c3,#5d6fa8,#4c578e,#3b4175,#2b2c5c)"
-			height="100%"
-		>
-			{props.children}
-		</Flex>
-	);
+  const path = usePathname();
+
+  return (
+    <Flex
+      direction="column"
+      bgGradient={
+        path == "/"
+          ? "linear(to-br, #9494ee, #8f98ee, #8c9ded,#89a0ec,#87a4ea,#7f9add,#7691d0,#6e87c3,#5d6fa8,#4c578e,#3b4175,#2b2c5c)"
+          : "linear(to-br, #282c34, #282c34)"
+      }
+      height="100%"
+    >
+      {props.children}
+    </Flex>
+  );
 };
 
 export default Container;

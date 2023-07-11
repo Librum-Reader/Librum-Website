46d84da58139ea621384237ccdbb6c0b321d70ba
odics <odics@vcu.edu>
2023-07-11 22:46:15 +0100

    Login and register now working

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,31 +5,36 @@ import Navbar from "./components/ui/Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import Container from "./components/ui/Container";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const lato = Lato({
-	subsets: ["latin"],
-	weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
 });
 
 const theme = extendTheme({
-	fonts: {
-		body: lato,
-	},
+  fonts: {
+    body: lato,
+  },
 });
 
+const queryClient = new QueryClient();
+
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				<ChakraProvider>
-					<Container>
-						<Navbar />
-						{children}
-					</Container>
-				</ChakraProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <ChakraProvider>
+            <Container>
+              <Navbar />
+              {children}
+            </Container>
+          </ChakraProvider>
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
 }

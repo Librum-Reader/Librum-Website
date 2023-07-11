704af16d3777625acd52441baa7403b56095b6de
odics <odics@vcu.edu>
2023-07-11 13:15:38 +0100

    Reduced width of navbar to match current website.

--- app/components/ui/Navbar.jsx
+++ app/components/ui/Navbar.jsx
@@ -5,50 +5,50 @@ import Link from "next/link";
 import LoginButton from "../ui/LoginButton";
 
 const navLinks = [
-	{
-		href: "/",
-		text: "HOME",
-	},
-	{
-		href: "/contact",
-		text: "CONTACT",
-	},
-	{
-		href: "/news",
-		text: "NEWS",
-	},
+  {
+    href: "/",
+    text: "HOME",
+  },
+  {
+    href: "/contact",
+    text: "CONTACT",
+  },
+  {
+    href: "/news",
+    text: "NEWS",
+  },
 ];
 
 const navLinkComponents = navLinks.map((link, index) => {
-	return (
-		<Link key={index} href={link.href}>
-			<Button colorScheme="gray" variant="ghost">
-				{link.text}
-			</Button>
-		</Link>
-	);
+  return (
+    <Link key={index} href={link.href}>
+      <Button colorScheme="gray" variant="ghost">
+        {link.text}
+      </Button>
+    </Link>
+  );
 });
 
 const Navbar = () => {
-	return (
-		<Flex
-			pt={4}
-			w="100%"
-			pl={4}
-			pr={4}
-			justifyContent="center"
-			alignItems="center"
-			className="navbar"
-			pb={4}
-		>
-			<Image alt="librum logo" src="ereader1.png" />
-			<Spacer />
-			<Flex gap="2rem">
-				{navLinkComponents}
-				<LoginButton />
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex maxW="1300px" width="100%" mx="auto">
+      <Flex
+        pt={4}
+        width="100%"
+        justifyContent="center"
+        alignItems="center"
+        className="navbar"
+        pb={4}
+      >
+        <Image alt="librum logo" src="ereader1.png" />
+        <Spacer />
+        <Flex gap="2rem">
+          {navLinkComponents}
+          <LoginButton />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Navbar;

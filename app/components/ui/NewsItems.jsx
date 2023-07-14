dbe4f5a8bfe39d8b3586e315ad5c6c428c005c6a
odics <odics@vcu.edu>
2023-07-14 14:08:36 +0100

    Contact form now works.

--- app/components/ui/NewsItems.jsx
+++ app/components/ui/NewsItems.jsx
@@ -1,41 +1,41 @@
 "use client";
 import {
-	Flex,
-	Heading,
-	Image,
-	Text,
-	Button,
-	VStack,
-	Card,
-	CardBody,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  Button,
+  VStack,
+  Card,
+  CardBody,
 } from "@chakra-ui/react";
 
 const NewsItems = (props) => {
-	return (
-		<Card bgColor="gray.700" borderRadius="3xl">
-			<CardBody>
-				<Flex maxW="1300px" gap="4rem" align="center">
-					<Image
-						alt="news item illustration"
-						src="/news/news.svg"
-						className="news-image"
-					/>
-					<VStack direction="column" spacing={2} align="flex-start">
-						<Heading size="lg" color="#946bde">
-							{props.title}
-						</Heading>
-						<Text color="red" fontWeight="bold">
-							{props.date}
-						</Text>
-						<Text color="white">{props.body}</Text>
-						<Button mt={2} alignSelf="self-start">
-							Read More
-						</Button>
-					</VStack>
-				</Flex>
-			</CardBody>
-		</Card>
-	);
+  return (
+    <Card bgColor="gray.700" borderRadius="sm">
+      <CardBody>
+        <Flex maxW="1300px" gap="4rem" align="center">
+          <Image
+            alt="news item illustration"
+            src="/news/news.svg"
+            className="news-image"
+          />
+          <VStack direction="column" spacing={2} align="flex-start">
+            <Heading size="lg" color="#946bde">
+              {props.title}
+            </Heading>
+            <Text color="red" fontWeight="bold">
+              {props.date}
+            </Text>
+            <Text color="white">{props.body}</Text>
+            <Button mt={2} alignSelf="self-start">
+              Read More
+            </Button>
+          </VStack>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default NewsItems;
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,21 +1,25 @@
 "use client";
-import { Flex, Heading, Text, Box } from "@chakra-ui/react";
-import Form from "../components/ui/Form";
+import { Flex, Heading, Text, Box, Card, CardBody } from "@chakra-ui/react";
+import ContactForm from "../components/ui/ContactForm";
 const Contact = () => {
-	return (
-		<Flex background="#282c34" align="center" direction="column" h="100vh">
-			<Box>
-				<Heading size="2xl" color="#946bde" mt="10rem" mb="2rem">
-					Need Help?
-				</Heading>
-				<Text fontSize="xl" color="white" mb="2rem">
-					Reach us through the form below, or at help@librumreader.com if you
-					have any questions, we are 100% responsive and reply quickly!
-				</Text>
-				<Form />
-			</Box>
-		</Flex>
-	);
+  return (
+    <Flex background="#282c34" align="center" direction="column" h="100vh">
+      <Card bgColor="gray.700" borderRadius="md" mt="2rem" maxW="30%">
+        <CardBody p="2rem">
+          <Box>
+            <Heading size="xl" color="#946bde" mb="2rem">
+              Need Help?
+            </Heading>
+            <Text fontSize="l" color="white" mb="2rem">
+              Reach us through the form below, or at help@librumreader.com if
+              you have any questions, we are 100% responsive and reply quickly!
+            </Text>
+            <ContactForm />
+          </Box>
+        </CardBody>
+      </Card>
+    </Flex>
+  );
 };
 
 export default Contact;

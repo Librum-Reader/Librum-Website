494e555ae56d14ad3a709f74053a85ec158d3ca6
Christian Moronta <cmoronta26@gmail.com>
2023-07-17 01:20:32 -0400

    Add ComingSoon section

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,14 @@
 import Image from "next/image";
 import Showcase from "./components/sections/Showcase";
 import Features from "./components/sections/Features";
-import { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import ComingSoon from "./components/sections/ComingSoon";
 
 export default function Home() {
   return (
     <>
       <Showcase />
       <Features />
+      <ComingSoon />
     </>
   );
 }

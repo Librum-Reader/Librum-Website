094aed2f09afc80f69618d8a41fef4e6012c0ba4
odics <odics@vcu.edu>
2023-07-25 21:35:39 +0100

    Changed news website, fixed footer border and main container

--- app/components/sections/Footer.jsx
+++ app/components/sections/Footer.jsx
@@ -12,29 +12,40 @@ import {
 
 const Footer = () => {
   return (
-    <VStack backgroundColor="bg-default" pb="1rem">
-      <Heading size="lg" mb="1rem">
-        Have any questions or concerns?
-      </Heading>
-      <Input
-        bgColor="whitesmoke"
-        type="email"
-        placeholder="Enter your email address"
-        w="32rem"
-        mb="1rem"
-      />
-      <Textarea
-        bgColor="whitesmoke"
-        placeholder="Message"
-        w="32rem"
-        mb="1rem"
-      />
-      <Button variant="primary">Send Message</Button>
-      <Flex w="100%" align="center">
+    <Flex
+      justify="center"
+      w="100%"
+      flexDirection="column"
+      borderTop="1px"
+      borderColor="text-default"
+      borderStyle="dotted"
+      pt="2rem"
+      mt="1rem"
+    >
+      <VStack backgroundColor="bg-default" pb="1rem">
+        <Heading size="lg" mb="1rem">
+          Have any questions or concerns?
+        </Heading>
+        <Input
+          bgColor="whitesmoke"
+          type="email"
+          placeholder="Enter your email address"
+          w="32rem"
+          mb="1rem"
+        />
+        <Textarea
+          bgColor="whitesmoke"
+          placeholder="Message"
+          w="32rem"
+          mb="1rem"
+        />
+        <Button variant="primary">Send Message</Button>
+      </VStack>
+      <Flex w="100%" align="center" justify="flex-start">
         <Image alt="librum logo" src="ereader1.png" />
         <Heading size="md">Librum</Heading>
       </Flex>
-    </VStack>
+    </Flex>
   );
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,6 @@ export default function Home() {
       <Features />
       {/* <ComingSoon /> */}
       {/* <Contribute /> */}
-      <Footer />
     </>
   );
 }

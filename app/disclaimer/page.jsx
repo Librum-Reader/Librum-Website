abd58b55f765f47ea749e57733271a6d2bb9b09a
Christian Moronta <cmoronta26@gmail.com>
2023-07-27 01:21:15 -0400

    Add disclaimer page

---
+++ app/disclaimer/page.jsx
@@ -0,0 +1,114 @@
+"use client";
+import { VStack, Heading, Text } from "@chakra-ui/react";
+
+const Disclaimer = (props) => {
+  return (
+    <VStack mt={8} mb={8} ml={24} mr={24} spacing={4} align="flex-start">
+      <Heading size="xl" color="purple.400">
+        Disclaimer
+      </Heading>
+      <Text>Last Updated: July 26, 2023</Text>
+      <Text>
+        {`Please carefully read this disclaimer before using Librum (the
+        "Application" and "Website"). Your use of the Application, its website,
+        and any features, content, or services made available by or through the
+        Application is subject to this disclaimer.`}
+      </Text>
+      <Heading size="md">Purpose of Librum</Heading>
+      <Text>
+        Librum utilizes open-source software under the terms of the GNU General
+        Public License version 3 (GPLv3) for certain aspects of the Application.
+        The provisions of the GPLv3 license apply to the respective open-source
+        components and govern their use, modification, and distribution. Please
+        refer to the applicable licenses for more information.
+      </Text>
+      <Text>
+        Librum is a platform for storing and reading e-books and other digital
+        content. The main goal of the application is to provide a simple and
+        straightforward way to access your library and read your books from
+        multiple devices.
+      </Text>
+      <Heading size="md">Copyrighted Materials</Heading>
+      <Text>
+        We respect the intellectual property rights of others and expect our
+        users to do the same.
+      </Text>
+      <Text>
+        The Application does not host or provide any copyrighted materials
+        without proper authorization. Users are responsible for ensuring that
+        the content they download or share through the Application complies with
+        applicable copyright laws.
+      </Text>
+      <Text>
+        The Application does not host or provide any copyrighted materials
+        without proper authorization. Users are responsible for ensuring that
+        the content they download or share through the Application complies with
+        applicable copyright laws.
+      </Text>
+      <Text>
+        Whereas all the books uploaded over the Application are under the
+        copyrights of the original authors and publishers. Therefore, we do not
+        own any copyright for the books. We are merely a medium for users to
+        read books under one platform.
+      </Text>
+      <Heading size="md">User Responsibilities</Heading>
+      <Text>
+        You understand and agree that your use of the Application or website and
+        any content you download or distribute are your responsibility as the
+        application user. You agree not to use the Application and website for
+        any illegal or infringing acts as you know that the unauthorised
+        distribution, replication, or sharing of copyrighted materials may
+        constitute unlawful activity.
+      </Text>
+      <Text>
+        {`It is necessary that all the content uploaded by the user should not be
+        unlawful or in breach of any person’s legal rights (such as, but not
+        only copyright) under any applicable law, or in any way that is
+        offensive, indecent, discriminatory, or otherwise objectionable.`}
+      </Text>
+      <Heading size="md">Data Collection and Privacy</Heading>
+      <Text>
+        The Application may collect certain user data, such as reading
+        statistics, preferences, and other usage information, to provide
+        personalized experiences and improve our services. We are committed to
+        protecting your privacy and handling your data in accordance with
+        applicable laws and our Privacy Policy. By using the Application, you
+        consent to collecting, storing, and processing your data as described in
+        our Privacy Policy.
+      </Text>
+      <Heading size="md">Limitation of Liability</Heading>
+      <Text>
+        {`The Application and its content are provided on an "as-is" and
+        "as-available" basis. We make no representations or warranties of any
+        kind, express or implied, regarding the application's accuracy,
+        reliability, or availability or its content. To the fullest extent
+        permitted by law, we disclaim all warranties, including but not limited
+        to the implied warranties of merchantability, fitness for a particular
+        purpose, and non-infringement. We shall not be liable for any direct,
+        indirect, incidental, consequential, or punitive damages arising out of
+        or in connection with the use or inability to use the Application.`}
+      </Text>
+      <Heading size="md">Changes to Disclaimer</Heading>
+      <Text>
+        We reserve the right to modify, suspend, or discontinue the Application
+        or any part of it, including this disclaimer, at any time without prior
+        notice. Any changes to the disclaimer will be effective immediately upon
+        posting the revised version on our website.
+      </Text>
+      <Heading size="md">Legal Advice</Heading>
+      <Text>
+        This disclaimer is not intended to provide legal advice or replace the
+        need for professional legal counsel. We recommend consulting with a
+        qualified attorney if you have specific questions regarding copyright,
+        intellectual property, or other legal matters.
+      </Text>
+      <Text>
+        Using the Application, you acknowledge that you have read, understood,
+        and agreed to this disclaimer. If you do not agree with any part of this
+        disclaimer, please refrain from using the Application.
+      </Text>
+    </VStack>
+  );
+};
+
+export default Disclaimer;

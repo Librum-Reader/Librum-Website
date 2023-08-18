7cff766581abd9a30631d9cbd6b57cd4c6de51d4
odics <odics@vcu.edu>
2023-08-18 21:35:18 +0100

    Changed disclaimer layout

--- app/disclaimer/page.jsx
+++ app/disclaimer/page.jsx
@@ -1,20 +1,28 @@
 "use client";
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VStack, Heading, Text, Flex, Link } from "@chakra-ui/react";
 
 const Disclaimer = (props) => {
   return (
-    <VStack mt={8} mb={8} ml={24} mr={24} spacing={4} align="flex-start">
-      <Heading size="xl" color="purple.400">
-        Disclaimer
-      </Heading>
-      <Text>Last Updated: July 26, 2023</Text>
-      <Text>
+    <Flex
+      spacing={4}
+      color="whitesmoke"
+      align="flex-start"
+      direction="column"
+      px="8.5em"
+      py="5em"
+      mx="auto"
+    >
+      <Heading fontSize="2.5em">DISCLAIMER</Heading>
+      <Text mb="3.2em">Last Updated: July 26, 2023</Text>
+      <Text mb="3.2em">
         {`Please carefully read this disclaimer before using Librum (the
         "Application" and "Website"). Your use of the Application, its website,
         and any features, content, or services made available by or through the
         Application is subject to this disclaimer.`}
       </Text>
-      <Heading size="md">Purpose of Librum</Heading>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Purpose of Librum
+      </Heading>
       <Text>
         Librum utilizes open-source software under the terms of the GNU General
         Public License version 3 (GPLv3) for certain aspects of the Application.
@@ -22,13 +30,15 @@ const Disclaimer = (props) => {
         components and govern their use, modification, and distribution. Please
         refer to the applicable licenses for more information.
       </Text>
-      <Text>
+      <Text mb="3.2em">
         Librum is a platform for storing and reading e-books and other digital
         content. The main goal of the application is to provide a simple and
         straightforward way to access your library and read your books from
         multiple devices.
       </Text>
-      <Heading size="md">Copyrighted Materials</Heading>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Copyrighted Materials
+      </Heading>
       <Text>
         We respect the intellectual property rights of others and expect our
         users to do the same.
@@ -39,19 +49,15 @@ const Disclaimer = (props) => {
         the content they download or share through the Application complies with
         applicable copyright laws.
       </Text>
-      <Text>
-        The Application does not host or provide any copyrighted materials
-        without proper authorization. Users are responsible for ensuring that
-        the content they download or share through the Application complies with
-        applicable copyright laws.
-      </Text>
-      <Text>
+      <Text mb="3.2em">
         Whereas all the books uploaded over the Application are under the
         copyrights of the original authors and publishers. Therefore, we do not
         own any copyright for the books. We are merely a medium for users to
         read books under one platform.
       </Text>
-      <Heading size="md">User Responsibilities</Heading>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        User Responsibilities
+      </Heading>
       <Text>
         You understand and agree that your use of the Application or website and
         any content you download or distribute are your responsibility as the
@@ -60,14 +66,16 @@ const Disclaimer = (props) => {
         distribution, replication, or sharing of copyrighted materials may
         constitute unlawful activity.
       </Text>
-      <Text>
+      <Text mb="3.2em">
         {`It is necessary that all the content uploaded by the user should not be
         unlawful or in breach of any person’s legal rights (such as, but not
         only copyright) under any applicable law, or in any way that is
         offensive, indecent, discriminatory, or otherwise objectionable.`}
       </Text>
-      <Heading size="md">Data Collection and Privacy</Heading>
-      <Text>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Data Collection and Privacy
+      </Heading>
+      <Text mb="3.2em">
         The Application may collect certain user data, such as reading
         statistics, preferences, and other usage information, to provide
         personalized experiences and improve our services. We are committed to
@@ -76,8 +84,10 @@ const Disclaimer = (props) => {
         consent to collecting, storing, and processing your data as described in
         our Privacy Policy.
       </Text>
-      <Heading size="md">Limitation of Liability</Heading>
-      <Text>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Limitation of Liability
+      </Heading>
+      <Text mb="3.2em">
         {`The Application and its content are provided on an "as-is" and
         "as-available" basis. We make no representations or warranties of any
         kind, express or implied, regarding the application's accuracy,
@@ -88,26 +98,41 @@ const Disclaimer = (props) => {
         indirect, incidental, consequential, or punitive damages arising out of
         or in connection with the use or inability to use the Application.`}
       </Text>
-      <Heading size="md">Changes to Disclaimer</Heading>
-      <Text>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Changes to Disclaimer
+      </Heading>
+      <Text mb="3.2em">
         We reserve the right to modify, suspend, or discontinue the Application
         or any part of it, including this disclaimer, at any time without prior
         notice. Any changes to the disclaimer will be effective immediately upon
         posting the revised version on our website.
       </Text>
-      <Heading size="md">Legal Advice</Heading>
+      <Heading fontSize="1.5em" textColor="white" mb=".5em">
+        Legal Advice
+      </Heading>
       <Text>
         This disclaimer is not intended to provide legal advice or replace the
         need for professional legal counsel. We recommend consulting with a
         qualified attorney if you have specific questions regarding copyright,
         intellectual property, or other legal matters.
       </Text>
-      <Text>
+      <Text mb="3.2em">
         Using the Application, you acknowledge that you have read, understood,
         and agreed to this disclaimer. If you do not agree with any part of this
         disclaimer, please refrain from using the Application.
       </Text>
-    </VStack>
+      <Heading fontSize="1.5em" mt="2em" mb=".5em" textColor="white">
+        Contact Us:
+      </Heading>
+      <Text>
+        For questions, you can reach us under:{" "}
+        <Link color="purple.400">help@librumreader.com</Link>
+      </Text>
+      <Text>
+        For business-related contact, reach out to us here:{" "}
+        <Link color="purple.400">contact@librumreader.com</Link>
+      </Text>
+    </Flex>
   );
 };
 

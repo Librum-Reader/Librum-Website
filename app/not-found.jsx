969250d629864d202615338145ace8e28fa8d304
odics <odics@vcu.edu>
2023-08-18 11:53:07 +0100

    404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Container from "./components/ui/Container";
+import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Flex direction="column" w="100%" align="center" mt="6rem">
+        <Heading fontSize="6rem" fontWeight={700}>
+          404
+        </Heading>
+        <Text fontSize="2em" mb="2rem">
+          Whoops! Page not found
+        </Text>
+        <Image src="./not_found.svg" w="400px" mb="4rem" />
+      </Flex>
+    </Container>
+  );
+};
+
+export default NotFound;
--- app/providers.jsx
+++ app/providers.jsx
@@ -1,6 +1,11 @@
 "use client";
 
 import "@fontsource/lato";
+import "@fontsource/lato/100.css";
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css";
+import "@fontsource/lato/700.css";
+import "@fontsource/lato/900.css";
 
 import { CacheProvider } from "@emotion/react";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -87,6 +92,13 @@ export function Providers({ children }) {
     initialColorMode: "light",
     useSystemColorMode: false,
     semanticTokens,
+    styles: {
+      global: {
+        body: {
+          bg: "bg-default",
+        },
+      },
+    },
     fonts: {
       body: `'Lato', sans-serif`,
       heading: `'Lato', sans-serif`,

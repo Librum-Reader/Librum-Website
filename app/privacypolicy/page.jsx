b33bc5623f3d3e1b0f150fa9101314f9ce12d10f
odics <odics@vcu.edu>
2023-08-19 11:25:50 +0100

    Fixed list items in privacy policy

--- app/privacypolicy/page.jsx
+++ app/privacypolicy/page.jsx
@@ -221,37 +221,37 @@ const PrivacyPolicy = (props) => {
       <Text>
         We may share Your personal information in the following situations:
       </Text>
-      <UnorderedList>
-        <ListItem>
+      <UnorderedList styleType="lower-alpha">
+        <ListItem ml="1em">
           With Service Providers: We may share Your personal information with
           Service Providers to monitor and analyze the use of our Service, to
           contact You. It also includes that SSL banking service for safe
           transactions and to protect your banking details.
         </ListItem>
-        <ListItem>
+        <ListItem ml="1em">
           For business transfers: We may share or transfer Your personal
           information in connection with, or during negotiations of, any merger,
           sale of Company assets, financing, or acquisition of all or a portion
           of Our business to another company.
         </ListItem>
-        <ListItem>
+        <ListItem ml="1em">
           With Affiliates: We may share Your information with Our affiliates, in
           which case we will require those affiliates to honor this Privacy
           Policy. Affiliates include Our parent company and any other
           subsidiaries, joint venture partners or other companies that We
           control or that are under common control with Us.
         </ListItem>
-        <ListItem>
+        <ListItem ml="1em">
           With business partners: We may share Your information with Our
           business partners to offer You certain products, services or
           promotions.
         </ListItem>
-        <ListItem>
+        <ListItem ml="1em">
           With other users: when You share personal information or otherwise
           interact in the public areas with other users, such information may be
           viewed by all users and may be publicly distributed outside.
         </ListItem>
-        <ListItem>
+        <ListItem ml="1em">
           With Your consent: We may disclose Your personal information for any
           other purpose with Your consent.
         </ListItem>

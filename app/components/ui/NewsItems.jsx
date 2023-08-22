a1895ce8781882d350862f072a6a1aab6987f9a4
odics <odics@vcu.edu>
2023-08-22 09:31:39 +0100

    Mobile news

--- app/components/ui/NewsItems.jsx
+++ app/components/ui/NewsItems.jsx
@@ -12,13 +12,18 @@ import {
 
 const NewsItems = (props) => {
   return (
-    <Card bgColor="bg-default" borderRadius="sm">
+    <Card
+      bgColor="bg-default"
+      borderRadius="sm"
+      mx={{ base: "1rem", md: "none" }}
+    >
       <CardBody>
         <Flex maxW="1300px" gap="4rem" align="center">
           <Image
             alt="news item illustration"
             src="/news/news.svg"
             className="news-image"
+            display={{ base: "none", md: "block" }}
           />
           <VStack direction="column" spacing={2} align="flex-start">
             <Heading size="lg" color="#946bde">

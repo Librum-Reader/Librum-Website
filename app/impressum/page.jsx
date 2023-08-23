e696d89bb57e468811f95e30b66a276f3f01710d
Christian Moronta <cmoronta26@gmail.com>
2023-08-23 14:39:54 -0400

    Fix impressum page spacing

--- app/impressum/page.jsx
+++ app/impressum/page.jsx
@@ -5,34 +5,40 @@ import { Flex, Heading, Text, VStack, Link } from "@chakra-ui/react";
 
 const Impressum = () => {
   return (
-    <Flex
+    <VStack
+      align="flex-start"
       w="100%"
       maxW="1300px"
       px={{ base: "0", md: "8.5em" }}
       py={{ base: "0", md: " 9em" }}
       mx={{ base: "1rem", md: "auto" }}
       h="100dvh"
+      spacing={1}
     >
-      <VStack mt={{ base: "1rem", md: "10rem" }} align="start">
-        <Heading mb="2rem">About Us</Heading>
-        <Text>Librum-Reader</Text>
-        <Text>Kirchenkamp 2, 50226, Frechen Germany</Text>
-        <Heading mb="2rem" mt="6rem">
-          Contact
-        </Heading>
-        <Text>Owner: David Lazarescu</Text>
-        <Text>
-          E-Mail:{" "}
-          <Link href="mailto:contact@librumreader.com">
-            contact@librumreader.com
-          </Link>
-        </Text>
-        <Text>
-          Internet:{" "}
-          <Link href="https://librumreader.com">https://librumreader.com</Link>
-        </Text>
+      <VStack spacing={16} mt={{ base: "4rem", md: "10rem" }} align="start">
+        <VStack align="flex-start">
+          <Heading mb="1.5rem">About Us</Heading>
+          <Text>Librum-Reader</Text>
+          <Text>Kirchenkamp 2, 50226, Frechen Germany</Text>
+        </VStack>
+        <VStack align="flex-start">
+          <Heading mb="2rem">Contact</Heading>
+          <Text>Owner: David Lazarescu</Text>
+          <Text>
+            E-Mail:{" "}
+            <Link href="mailto:contact@librumreader.com">
+              contact@librumreader.com
+            </Link>
+          </Text>
+          <Text>
+            Internet:{" "}
+            <Link href="https://librumreader.com">
+              https://librumreader.com
+            </Link>
+          </Text>
+        </VStack>
       </VStack>
-    </Flex>
+    </VStack>
   );
 };
 

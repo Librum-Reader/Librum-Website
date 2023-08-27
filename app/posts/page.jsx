4868d432db7a0181af2c7530033a95988449c000
odics <odics@vcu.edu>
2023-08-27 23:06:34 +0100

    Sanity posting

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -3,10 +3,48 @@
 import React from "react";
 import { Flex, Box, Heading, VStack, Text } from "@chakra-ui/react";
 import { useSearchParams } from "next/navigation";
+import { createClient } from "next-sanity";
+import { useState, useEffect } from "react";
+import { PortableText } from "@portabletext/react";
 
 const Posts = () => {
   const searchParams = useSearchParams();
   const postId = searchParams.get("id");
+  const client = createClient({
+    projectId: "46vwrypj",
+    dataset: "production",
+    apiVersion: "2023-08-27",
+    useCdn: false,
+  });
+
+  const fetchPost = async () => {
+    const query = `*[_id == $postId][0] | {
+        _id,
+        title,
+        publishedAt,
+        'slug': slug.current,
+        body,
+        summary
+      }`;
+    const params = { postId };
+
+    try {
+      const posts = await client.fetch(query, params);
+      return posts;
+    } catch {
+      console.error("Error retrieving posts.");
+    }
+  };
+  const [post, setPost] = useState();
+
+  useEffect(() => {
+    const fetchedPost = fetchPost();
+    fetchedPost.then((post) => {
+      setPost(post);
+      console.log(post.body);
+    });
+  }, []);
+
   return (
     <Flex background="bg-default" align="center" direction="column">
       <Box>
@@ -16,11 +54,13 @@ const Posts = () => {
           mt={{ base: "2rem", md: "24" }}
           mb={{ base: "2rem", md: "24" }}
         >
-          News and Updates
+          {post?.title}
         </Heading>
       </Box>
       <VStack spacing={8} mb={8}>
-        <Text>{postId}</Text>
+        <Text>
+          <PortableText value={post?.body} />
+        </Text>
       </VStack>
     </Flex>
   );

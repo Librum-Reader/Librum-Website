15ef97c4cc8950522555f0c7c0a79460dc441327
DavidLazarescu <prtnprvtmail@protonmail.com>
2023-08-29 00:29:38 +0200

    Removed src/

--- src/Components/Profile/ProfilePage.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ProfilePage = () => {
-  return <div className="container">Profile</div>;
-};
--- src/Components/ThemeToggle/ThemeToggle.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./ThemeToggle.css";
-
-export const ThemeToggle = () => {
-  return <div className="themeToggle-container">ThemeToggle</div>;
-};

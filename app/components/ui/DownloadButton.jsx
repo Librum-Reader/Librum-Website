d673e750529fe432442b7ba8889fbf3612d22b13
odics <odics@vcu.edu>
2023-09-01 21:24:08 +0100

    Download for Linux

--- app/components/ui/DownloadButton.jsx
+++ app/components/ui/DownloadButton.jsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  forwardRef,
+} from "@chakra-ui/react";
 import Link from "next/link";
+import { FaLinux, FaCopy } from "react-icons/fa";
 import { AiOutlineWindows } from "react-icons/ai";
+import { SiFlatpak } from "react-icons/si";
 
 const DownloadButton = () => {
   const [os, setOS] = useState("");
@@ -35,6 +48,28 @@ const DownloadButton = () => {
     detectOS();
   }, []);
 
+  async function copyTextToClipboard() {
+    if ("clipboard" in navigator) {
+      return await navigator.clipboard.writeText(
+        "flatpak install flathub com.librumreader.librum"
+      );
+    } else {
+      return document.execCommand(
+        "copy",
+        true,
+        "flatpak install flathub com.librumreader.librum"
+      );
+    }
+  }
+
+  const CustomButton = forwardRef((props, ref) => {
+    return (
+      <Button variant="primary" w="250px" ref={ref}>
+        Flatpak
+      </Button>
+    );
+  });
+
   if (os === "Windows") {
     return (
       <Link href="https://librumblobstorage.blob.core.windows.net/binaries/windows_installer_0-6-3.exe">
@@ -43,9 +78,41 @@ const DownloadButton = () => {
           variant="primary"
           leftIcon={<AiOutlineWindows size={18} />}
         >
-          Download for Windows
+          Download
         </Button>
       </Link>
+    );
+  } else if (os === "Linux") {
+    return (
+      <Menu>
+        <MenuButton>
+          <Button variant="primary" w="250px" leftIcon={<FaLinux />}>
+            Download
+          </Button>
+        </MenuButton>
+        <MenuList>
+          <MenuItem
+            onClick={() =>
+              window.open(
+                "https://librumstorageaccount.blob.core.windows.net/releases/0-6-3-linux-binaries.tar.gz",
+                "_blank"
+              )
+            }
+          >
+            tar.gz
+          </MenuItem>
+          <MenuItem
+            onClick={() =>
+              window.open(
+                "https://flathub.org/apps/com.librumreader.librum",
+                "_blank"
+              )
+            }
+          >
+            Flatpak
+          </MenuItem>
+        </MenuList>
+      </Menu>
     );
   } else {
     return (

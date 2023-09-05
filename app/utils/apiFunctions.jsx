9525b6872d333e02d6fe728d14203cbb7f558f06
odics <odics@vcu.edu>
2023-09-05 15:18:32 +0100

    Fixed API endpoint for user registration

--- app/utils/apiFunctions.jsx
+++ app/utils/apiFunctions.jsx
@@ -32,20 +32,23 @@ export const userRegistration = async (data) => {
   try {
     console.log(data);
 
-    const response = await fetch("https://api.librumreader.com/register", {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        //   Authorization: `Bearer ${token}`,
-      },
-      method: "POST",
-      body: JSON.stringify({
-        FirstName: data.FirstName,
-        LastName: data.LastName,
-        Email: data.Email,
-        Password: data.Password,
-      }),
-    });
+    const response = await fetch(
+      "https://api.librumreader.com/authentication/register",
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          //   Authorization: `Bearer ${token}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          FirstName: data.FirstName,
+          LastName: data.LastName,
+          Email: data.Email,
+          Password: data.Password,
+        }),
+      }
+    );
 
     const result = await response;
     console.log(result);

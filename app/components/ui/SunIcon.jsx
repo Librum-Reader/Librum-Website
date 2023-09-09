c0598fbf812bd05f8dd38e2ec2a893d1341d8ad9
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2023-09-09 12:10:48 +0330

    Some warnings related to svg in SunIcon.jsx resolved

--- app/components/ui/SunIcon.jsx
+++ app/components/ui/SunIcon.jsx
@@ -8,71 +8,70 @@ const SunIcon = () => {
       height="18"
       viewBox="0 0 21 21"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g clip-path="url(#clip0_1686_3420)">
+      xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_1686_3420)">
         <path
           d="M10.5 14.875C12.9162 14.875 14.875 12.9162 14.875 10.5C14.875 8.08375 12.9162 6.125 10.5 6.125C8.08375 6.125 6.125 8.08375 6.125 10.5C6.125 12.9162 8.08375 14.875 10.5 14.875Z"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.5 0.875V2.625"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.5 18.375V20.125"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3.6925 3.69238L4.935 4.93488"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.065 16.0649L17.3075 17.3074"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M0.875 10.5H2.625"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M18.375 10.5H20.125"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M3.6925 17.3074L4.935 16.0649"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16.065 4.93488L17.3075 3.69238"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>

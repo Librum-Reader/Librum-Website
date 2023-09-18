93bd2e58785b2ee221462d4df7916347aa0bb684
odics <odics@vcu.edu>
2023-09-18 20:20:23 +0100

    Add support for heading tags in posts

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -69,6 +69,20 @@ const Posts = () => {
       p: ({ children }) => {
         return <Text mb="1rem">{children}</Text>;
       },
+      h1: ({ children }) => {
+        return (
+          <Heading size={{ base: "lg", md: "xl" }} color="#946bde">
+            {children}
+          </Heading>
+        );
+      },
+      h2: ({ children }) => {
+        return (
+          <Heading size={{ base: "md", md: "lg" }} color="#946bde">
+            {children}
+          </Heading>
+        );
+      },
     },
   };
 

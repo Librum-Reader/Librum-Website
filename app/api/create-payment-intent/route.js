b8b34ff55ff561e60255ee7acf2c20003b47ed97
odics <odics@vcu.edu>
2023-10-13 21:46:48 +0100

    Add netlify config for API routes

--- app/api/create-payment-intent/route.js
+++ app/api/create-payment-intent/route.js
@@ -7,23 +7,17 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export async function POST(req, res) {
   const { amount: donationAmount } = await req.json();
-  console.log("API", donationAmount);
-  // request = await req.body;
-  // console.log(request);
-  // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: donationAmount * 100,
     currency: "eur",
-    // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
     automatic_payment_methods: {
       enabled: true,
     },
   });
 
-  const response = JSON.stringify({
-    clientSecret: paymentIntent.client_secret,
-  });
-
-  console.log(response);
   return Response.json({ clientSecret: paymentIntent.client_secret });
 }
+
+export const config = {
+  type: "experimental-background",
+};

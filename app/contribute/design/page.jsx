c42082190b84ea076e27c00df9ab0dcc8f0b5354
odics <odics@vcu.edu>
2023-11-05 20:30:58 +0000

    Add illustration to design contribution page

--- app/contribute/design/page.jsx
+++ app/contribute/design/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React from "react";
-import { Flex, Text, Heading, useMediaQuery, Link } from "@chakra-ui/react";
-import { AiFillGithub } from "react-icons/ai";
-import { FaPatreon } from "react-icons/fa";
-import { CgFigma } from "react-icons/cg";
-import { PiPatreonLogoFill } from "react-icons/pi";
-import { BiSolidDonateHeart } from "react-icons/bi";
-import { IoPeopleSharp } from "react-icons/io5";
+import {
+  Flex,
+  Text,
+  Heading,
+  useMediaQuery,
+  Link,
+  Image,
+} from "@chakra-ui/react";
 
 const Design = () => {
   return (
@@ -22,46 +23,50 @@ const Design = () => {
       direction={{ base: "column", md: "row" }}
       justify="center"
     >
-      <Flex w={{ base: "100%", md: "80%" }} direction="column">
-        <Flex align="center" gap="1rem" mb="2rem">
-          <Heading m="0">Contribute through design</Heading>
-        </Flex>
-        <Flex direction="column" gap="1rem">
-          <Text>
-            We are constantly working on new features or on improving old ones.
-          </Text>
-          <Text>
-            All of these tasks require a fresh and innovative perspective to
-            ensure that our software remains cutting-edge and user-friendly.
-            That's where you come in! We invite talented designers to join our
-            open-source community and help us shape the future of Librum's user
-            interface.
-          </Text>
-          <Text>
-            By contributing to our project, you'll have the opportunity to
-            showcase your creativity and design skills to a global audience.
-            Whether you're an experienced designer or just starting your
-            journey, there's a place for you in our community.
-          </Text>
-          <Text>
-            If you are interested in contributing to Librum's design, feel free
-            to shoot us an email at{" "}
-            <Link href="mailto:contact@librumreader.com" textColor="#946BDE">
-              contact@librumreader.com
-            </Link>{" "}
-            or use the{" "}
-            <Link
-              textColor="#946BDE"
-              href=""
-              onClick={(e) => {
-                e.preventDefault();
-                window.scrollTo(0, document.body.scrollHeight);
-              }}
-            >
-              contact
-            </Link>{" "}
-            form below.
-          </Text>
+      <Flex justify="space-between" gap="4rem" alignSelf="flex-start">
+        <Image src="/mobile_design.svg" width="40%" alignSelf="center" />
+        <Flex w={{ base: "100%", md: "80%" }} direction="column">
+          <Flex align="center" gap="1rem" mb="2rem">
+            <Heading m="0">Contribute through design</Heading>
+          </Flex>
+          <Flex direction="column" gap="1rem">
+            <Text>
+              We are constantly working on new features or on improving old
+              ones.
+            </Text>
+            <Text>
+              All of these tasks require a fresh and innovative perspective to
+              ensure that our software remains cutting-edge and user-friendly.
+              That's where you come in! We invite talented designers to join our
+              open-source community and help us shape the future of Librum's
+              user interface.
+            </Text>
+            <Text>
+              By contributing to our project, you'll have the opportunity to
+              showcase your creativity and design skills to a global audience.
+              Whether you're an experienced designer or just starting your
+              journey, there's a place for you in our community.
+            </Text>
+            <Text>
+              If you are interested in contributing to Librum's design, feel
+              free to shoot us an email at{" "}
+              <Link href="mailto:contact@librumreader.com" textColor="#946BDE">
+                contact@librumreader.com
+              </Link>{" "}
+              or use the{" "}
+              <Link
+                textColor="#946BDE"
+                href=""
+                onClick={(e) => {
+                  e.preventDefault();
+                  window.scrollTo(0, document.body.scrollHeight);
+                }}
+              >
+                contact
+              </Link>{" "}
+              form below.
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>

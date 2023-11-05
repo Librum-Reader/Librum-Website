d2087be61ad9ab766c4ad4e90d229d1d34fcf755
David Lazarescu <69865187+DavidLazarescu@users.noreply.github.com>
2023-11-05 21:13:57 +0100

    Updated download button to get version 0.10.1

--- app/components/ui/DownloadButton.jsx
+++ app/components/ui/DownloadButton.jsx
@@ -75,7 +75,7 @@ const DownloadButton = () => {
   if (os === "Windows") {
     return (
       <a
-        href="https://librumstorageaccount.blob.core.windows.net/releases/librum-windows-0.10.0-installer.exe"
+        href="https://librumstorageaccount.blob.core.windows.net/releases/librum-windows-0.10.1-installer.exe"
         target="#"
       >
         <Button
